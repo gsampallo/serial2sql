0bccedc056d0ec1655ec256e12fded643bf920ad
Guillermo Sampallo <guille@gmail.com>
2019-07-17 15:46:50 -0300

    archivos no necesarios

--- test3.py
+++
@@ -1,5 +0,0 @@
-from serial2sql import serial2sql
-
-s = serial2sql("config.json")
-s.setOutput("output.sql")
-s.run()
--- test4.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open("config.json")as f:
-    param = json.load(f)
-
-print(param["tableName"])
-print(param["credentials"]["host"])
-print(param["credentials"]["user"])
-print(param["credentials"]["password"])
